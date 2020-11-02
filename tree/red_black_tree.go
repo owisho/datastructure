1c8b0f59c25c674f3c16ade198453949d8f508a0
wangyang <yangw@opera.com>
2020-11-02 18:28:38 +0800

    update red_black_tree insert code

--- tree/red_black_tree.go
+++ tree/red_black_tree.go
@@ -49,7 +49,7 @@ func (self *RedBlackTree) Insert(data int) (bool, *RedBlackTreeNode) {
 		}
 		return true, self.root
 	} else {
-		b, node := compareAndInsert(self.root, data)
+		b, node := self.root.Insert(data)
 		self.root = searchRoot(self.root)
 		return b, node
 	}
@@ -60,136 +60,6 @@ func searchRoot(node *RedBlackTreeNode) *RedBlackTreeNode {
 		return node
 	}
 	return searchRoot(node.parent)
-}
-
-func compareAndInsert(node *RedBlackTreeNode, data int) (bool, *RedBlackTreeNode) {
-	if data == node.data {
-		fmt.Println("data already existed in tree")
-		return false, node
-	}
-	if data < node.data {
-		lcnode := node.leftChild
-		if lcnode == nil {
-			nnode := &RedBlackTreeNode{
-				data:       data,
-				isRed:      true,
-				parent:     node,
-				leftChild:  nil,
-				rightChild: nil,
-			}
-			node.leftChild = nnode
-			adjustTree(nnode)
-			return true, nnode
-		} else {
-			return compareAndInsert(lcnode, data)
-		}
-	}
-	if data > node.data {
-		rcnode := node.rightChild
-		if rcnode == nil {
-			nnode := &RedBlackTreeNode{
-				data:       data,
-				isRed:      true,
-				parent:     node,
-				leftChild:  nil,
-				rightChild: nil,
-			}
-			node.rightChild = nnode
-			adjustTree(nnode)
-			return true, nnode
-		} else {
-			return compareAndInsert(node.rightChild, data)
-		}
-	}
-	fmt.Println("error status, all status should dealed before")
-	panic("error status")
-}
-
-//调整数，参数为新增加的红色节点
-func adjustTree(node *RedBlackTreeNode) {
-	if node.parent == nil {
-		node.isRed = false
-		return
-	}
-	if !node.parent.isRed {
-		//不需要做任何调整
-		return
-	}
-	// 存在父节点并且父节点的颜色为红色
-	parent := node.parent
-	uncle := getUncle(parent)
-	if uncle != nil && uncle.isRed {
-		uncle.isRed = false
-		parent.isRed = false
-		parent.parent.isRed = true
-		adjustTree(parent.parent)
-	} else {
-		gp := parent.parent
-		if (node.data < parent.data && parent.data < gp.data) ||
-			(node.data > parent.data && parent.data > gp.data) {
-
-			adjustGPParentChild(parent, gp)
-			if node.data < parent.data {
-				rightRotate(parent, gp)
-			} else {
-				leftRotate(parent, gp)
-			}
-		} else {
-			if node.data > parent.data { //因为node.data > parent.data和上面的if条件不成立，所以  parent.data < gp.data
-				node.parent = gp
-				parent.rightChild = node.leftChild
-				node.leftChild = parent
-				parent.parent = node
-				adjustGPParentChild(node, gp)
-				rightRotate(node, gp)
-			} else {
-				node.parent = gp
-				parent.leftChild = node.rightChild
-				node.rightChild = parent
-				parent.parent = node
-				adjustGPParentChild(node, gp)
-				leftRotate(node, gp)
-			}
-		}
-	}
-
-}
-
-func adjustGPParentChild(parent, gp *RedBlackTreeNode) {
-	if gp.parent == nil {
-		return
-	}
-	if gp.parent.rightChild != nil && gp.parent.rightChild.data == gp.data {
-		gp.parent.rightChild = parent
-	} else {
-		gp.parent.leftChild = parent
-	}
-}
-
-func rightRotate(parent, gp *RedBlackTreeNode) {
-	parent.parent = gp.parent
-	parent.isRed = false
-	gp.isRed = true
-	gp.parent = parent
-	gp.leftChild = parent.rightChild
-	parent.rightChild = gp
-}
-
-func leftRotate(parent, gp *RedBlackTreeNode) {
-	parent.parent = gp.parent
-	parent.isRed = false
-	gp.isRed = true
-	gp.parent = parent
-	gp.rightChild = parent.leftChild
-	parent.leftChild = gp
-}
-
-func getUncle(parent *RedBlackTreeNode) *RedBlackTreeNode {
-	if parent.data < parent.parent.data {
-		return parent.parent.rightChild
-	} else {
-		return parent.parent.leftChild
-	}
 }
 
 //搜索数据
--- tree/red_black_tree_node.go
+++ tree/red_black_tree_node.go
@@ -7,3 +7,137 @@ type RedBlackTreeNode struct {
 	data       int
 	isRed      bool
 }
+
+func (self *RedBlackTreeNode) Insert(data int) (bool, *RedBlackTreeNode) {
+	if data == self.data {
+		return false, self
+	} else if data < self.data {
+		if self.leftChild == nil {
+			nnode := &RedBlackTreeNode{
+				parent: self,
+				data:   data,
+				isRed:  true,
+			}
+			self.leftChild = nnode
+			nnode.adjust()
+			return true, nnode
+		} else {
+			return self.leftChild.Insert(data)
+		}
+	} else { // data > self.data
+		if self.rightChild == nil {
+			nnode := &RedBlackTreeNode{
+				parent: self,
+				data:   data,
+				isRed:  true,
+			}
+			self.rightChild = nnode
+			nnode.adjust()
+			return true, nnode
+		} else {
+			return self.rightChild.Insert(data)
+		}
+	}
+}
+
+//调整数，参数为新增加的红色节点
+func (self *RedBlackTreeNode) adjust() {
+	if self.parent == nil {
+		self.isRed = false
+		return
+	}
+	if !self.parent.isRed {
+		//不需要做任何调整
+		return
+	}
+	// 存在父节点并且父节点的颜色为红色
+	parent := self.parent
+	uncle := self.getUncle()
+	if uncle != nil && uncle.isRed { //叔父节点为红色
+		uncle.isRed = false
+		parent.isRed = false
+		parent.parent.isRed = true
+		parent.parent.adjust()
+	} else {
+		gp := parent.parent
+		if (self.data < parent.data && parent.data < gp.data) ||
+			(self.data > parent.data && parent.data > gp.data) {
+
+			if self.data < parent.data {
+				parent.rightRotate()
+			} else {
+				parent.leftRotate()
+			}
+		} else {
+			if self.data > parent.data { //因为self.data > parent.data和上面的if条件不成立，所以  parent.data < gp.data
+				self.parent = gp
+				parent.rightChild = self.leftChild
+				self.leftChild = parent
+				parent.parent = self
+				self.rightRotate()
+			} else {
+				self.parent = gp
+				parent.leftChild = self.rightChild
+				self.rightChild = parent
+				parent.parent = self
+				self.leftRotate()
+			}
+		}
+	}
+}
+
+//当前节点是左旋的中间节点
+func (self *RedBlackTreeNode) leftRotate() {
+	parent := self.parent
+	gp := parent.parent
+
+	//修改节点的祖父节点的子节点（将数据挂在祖父节点下）
+	if gp != nil {
+		if parent == gp.rightChild {
+			gp.rightChild = self
+		} else {
+			gp.leftChild = self
+		}
+	}
+
+	//
+	self.parent = gp
+	parent.parent = self
+	parent.rightChild = self.leftChild
+	self.leftChild = parent
+	parent.isRed = true
+	self.isRed = false
+
+}
+
+//当前节点是左旋的中间节点
+func (self *RedBlackTreeNode) rightRotate() {
+	parent := self.parent
+	gp := parent.parent
+
+	//修改节点的祖父节点的子节点（将数据挂在祖父节点下）
+	if gp != nil {
+		if parent == gp.rightChild {
+			gp.rightChild = self
+		} else {
+			gp.leftChild = self
+		}
+	}
+
+	self.parent = gp
+	parent.parent = self
+	parent.leftChild = self.rightChild
+	self.rightChild = parent
+	parent.isRed = true
+	self.isRed = false
+}
+
+func (self *RedBlackTreeNode) getUncle() *RedBlackTreeNode {
+	parent := self.parent
+	gp := parent.parent
+	if parent == gp.leftChild {
+		return gp.rightChild
+	} else {
+		return gp.leftChild
+	}
+}
